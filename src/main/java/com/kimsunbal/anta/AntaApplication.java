8bca5ce52ce3d9facf453a66384a0e9091b5dc23
kimsunbal <kimsunbal@naver.com>
2023-04-19 20:55:57 +0900

    init commit

---
+++ src/main/java/com/kimsunbal/anta/AntaApplication.java
@@ -0,0 +1,13 @@
+package com.kimsunbal.anta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AntaApplication.class, args);
+    }
+
+}
